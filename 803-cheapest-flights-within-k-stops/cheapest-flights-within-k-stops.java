3bab6c3279a7b003a64fec48619dcf61c86f91f1
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2025-12-30 12:22:14 +0530

    Time: 8 ms (39.63%) | Memory: 46.4 MB (77.58%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,66 @@
+class Solution {
+    HashMap<Integer,HashMap<Integer,Integer>> mp;
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        mp=new HashMap<>();
+        for(int i=0;i<n;i++){
+            mp.put(i,new HashMap<>());
+        }
+        for(int[] e:flights){
+            int v1=e[0];
+            int v2=e[1];
+            int cost=e[2];
+            mp.get(v1).put(v2,cost);
+        }
+        return path(dst,k,src);
+    }
+    public int path(int dst,int k,int src){
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.cost-b.cost);
+        pq.add(new Pair(src,0,k+1));
+        // HashSet<Integer> st=new HashSet<>();
+        int[] best = new int[mp.size()];
+        Arrays.fill(best, -1);
+
+        while(!pq.isEmpty()){
+            // remove
+            Pair rp=pq.poll();
+
+            // ignore
+            // if(st.contains(rp.vtx)){
+            //     continue;
+            // }
+            // // mark visited
+            // st.add(rp.vtx);
+
+            // self work
+            if(rp.vtx==dst){
+                return rp.cost;
+            }
+
+            if(best[rp.vtx] >= rp.mk){
+                continue;
+            }
+            best[rp.vtx] = rp.mk;
+
+            // add nbrs
+            if(rp.mk>0){
+                for(int nbrs:mp.get(rp.vtx).keySet()){
+                    // if(!st.contains(nbrs)){
+                        int cost=mp.get(rp.vtx).get(nbrs);
+                        pq.add(new Pair(nbrs,rp.cost+cost,rp.mk-1));
+                    // }
+                }
+            }
+        }
+        return -1;
+    }
+    class Pair{
+        int vtx;
+        int cost;
+        int mk;
+        Pair(int vtx,int cost,int mk){
+            this.vtx=vtx;
+            this.cost=cost;
+            this.mk=mk;
+        }
+    }
+}
