40355b438b23c6bca7e2eae3925108c2a8d841b3
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2026-03-19 15:44:09 +0530

    Time: 2278 ms (31.42%) | Memory: 42.8 MB (82.11%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int minCut(String s) {
+        int n=s.length();
+
+        // return solve(0,n,s)-1;
+        
+        int[] dp=new int[n];
+        Arrays.fill(dp,-1);
+
+        return solve2(0,n,s,dp)-1;
+    }
+
+    private int solve2(int i, int n, String s, int[] dp){
+        if(i==n){
+            return 0;
+        }
+
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+
+        int min=Integer.MAX_VALUE;
+
+        for(int j=i; j<n; j++){
+            if(ispal(i,j,s)){
+                int cost= 1 + solve2(j+1,n,s,dp);
+                min=Math.min(min, cost);
+            }
+        }
+
+        return dp[i]=min;
+    }
+
+    private int solve(int i, int n, String s){
+        if(i==n){
+            return 0;
+        }
+
+        int min=Integer.MAX_VALUE;
+
+        for(int j=i; j<n; j++){
+            if(ispal(i,j,s)){
+                int cost= 1 + solve(j+1,n,s);
+                min=Math.min(min, cost);
+            }
+        }
+
+        return min;
+    }
+
+    private boolean ispal(int i, int j, String s){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
