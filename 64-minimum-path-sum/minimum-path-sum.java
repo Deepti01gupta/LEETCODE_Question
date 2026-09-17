7677c4a12340f86d1882db0f024f1d77644e4fa8
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2026-09-17 23:45:58 +0530

    Time: 6 ms (9.54%) | Memory: 50.2 MB (56.73%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,25 +3,59 @@ class Solution {
         int n=grid.length;
         int m=grid[0].length;
         int[][] dp=new int[n][m];
-        for(int[] r:dp){
-            Arrays.fill(r, -1);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i==0 && j==0){
+                    dp[i][j]=grid[i][j];
+                }
+                else{
+                    int up=grid[i][j];
+                    int left=grid[i][j];
+                    if(i>0){
+                        up+=dp[i-1][j];
+                    }
+                    else{
+                        up=Integer.MAX_VALUE;
+                    }
+                    if(j>0){
+                        left+=dp[i][j-1];
+                    }
+                    else{
+                        left=Integer.MAX_VALUE;
+                    }
+
+                    dp[i][j]=Math.min(left, up);
+                }
+            }
         }
-        return solve(n-1, m-1, grid, dp);
+        return dp[n-1][m-1];
     }
-    public int solve(int n, int m, int[][] arr, int[][] dp){
-        if(n<0 || m<0){
-            return Integer.MAX_VALUE;
-        }
-        if(n==0 && m==0){
-            return arr[0][0];
-        }
-        if(dp[n][m]!=-1){
-            return dp[n][m];
-        }
-        int up=solve(n-1, m, arr, dp);
-        int left=solve(n, m-1, arr, dp);
-        return dp[n][m] = arr[n][m] + Math.min(up, left);
-    }
+
+
+
+    // public int minPathSum(int[][] grid) {
+    //     int n=grid.length;
+    //     int m=grid[0].length;
+    //     int[][] dp=new int[n][m];
+    //     for(int[] r:dp){
+    //         Arrays.fill(r, -1);
+    //     }
+    //     return solve(n-1, m-1, grid, dp);
+    // }
+    // public int solve(int n, int m, int[][] arr, int[][] dp){
+    //     if(n<0 || m<0){
+    //         return Integer.MAX_VALUE;
+    //     }
+    //     if(n==0 && m==0){
+    //         return arr[0][0];
+    //     }
+    //     if(dp[n][m]!=-1){
+    //         return dp[n][m];
+    //     }
+    //     int up=solve(n-1, m, arr, dp);
+    //     int left=solve(n, m-1, arr, dp);
+    //     return dp[n][m] = arr[n][m] + Math.min(up, left);
+    // }
 
 
 
