33b3af7f7f82e84a9ba3c5c823d14d56c8c5f9ec
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2026-09-17 12:39:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (56.66%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,33 +2,51 @@ class Solution {
     // public int rob(int[] nums) {
         
     // }
-    // public int rob(int[] nums) {
-        
-    // }
-
     public int rob(int[] nums) {
-        int[] dp=new int[nums.length];
-        Arrays.fill(dp,-1); 
+        int n=nums.length;
+        int[] dp=new int[n];
+        dp[0]=nums[0];
+        int neg=0;
 
-        return solve(nums.length-1, nums, dp);
+        for(int i=1; i<n; i++){
+            int take=nums[i];
+            if(i>1){
+                take+=dp[i-2];
+            }
+
+            int nottake=dp[i-1];
+
+            dp[i]=Math.max(take, nottake);
+        }
+
+        return dp[n-1];
     }
-    public int solve(int idx, int[] nums, int[] dp){
-        if(idx==0){
-            return nums[idx];
-        }
-        if(idx<0){
-            return 0;
-        }
 
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
 
-        int take=nums[idx] + solve(idx-2, nums, dp);
-        int nottake=solve(idx-1, nums, dp);
 
-        return dp[idx]=Math.max(take, nottake);
-    }
+    // public int rob(int[] nums) {
+    //     int[] dp=new int[nums.length];
+    //     Arrays.fill(dp,-1); 
+
+    //     return solve(nums.length-1, nums, dp);
+    // }
+    // public int solve(int idx, int[] nums, int[] dp){
+    //     if(idx==0){
+    //         return nums[idx];
+    //     }
+    //     if(idx<0){
+    //         return 0;
+    //     }
+
+    //     if(dp[idx]!=-1){
+    //         return dp[idx];
+    //     }
+
+    //     int take=nums[idx] + solve(idx-2, nums, dp);
+    //     int nottake=solve(idx-1, nums, dp);
+
+    //     return dp[idx]=Math.max(take, nottake);
+    // }
 
 
 
