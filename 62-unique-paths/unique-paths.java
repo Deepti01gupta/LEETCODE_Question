2aea33d03027fee1acaf960753c473f85598506d
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2026-09-17 22:27:48 +0530

    Time: 1 ms (13.42%) | Memory: 42.2 MB (47.56%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,34 +1,56 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
-        for(int[] r:dp){
-            Arrays.fill(r, -1);
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+
+                int up=0;
+                int left=0;
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
         }
 
-        return solve(m-1, n-1, dp);
+        return dp[m-1][n-1];
     }
 
-    public int solve(int m, int n, int[][] dp){
-        if(n==0 && m==0){
-            return 1;
-        }
-        if(n<0 || m<0){
-            return 0;
-        }
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
 
-        int up=solve(m-1, n, dp);
-        int left=solve(m, n-1, dp);
 
-        return dp[m][n]=up+left;
-    }
+    // public int uniquePaths(int m, int n) {
+    //     int[][] dp=new int[m][n];
+    //     for(int[] r:dp){
+    //         Arrays.fill(r, -1);
+    //     }
+
+    //     return solve(m-1, n-1, dp);
+    // }
+    // public int solve(int m, int n, int[][] dp){
+    //     if(n==0 && m==0){
+    //         return 1;
+    //     }
+    //     if(n<0 || m<0){
+    //         return 0;
+    //     }
+    //     if(dp[m][n]!=-1){
+    //         return dp[m][n];
+    //     }
+    //     int up=solve(m-1, n, dp);
+    //     int left=solve(m, n-1, dp);
+    //     return dp[m][n]=up+left;
+    // }
+
+
 
     // public int uniquePaths(int m, int n) {
     //     return solve(m-1, n-1);
     // }
-
     // public int solve(int n, int m){
     //     if(n==0 && m==0){
     //         return 1;
@@ -36,10 +58,8 @@ class Solution {
     //     if(n<0 || m<0){
     //         return 0;
     //     }
-
     //     int up=solve(m-1, n);
     //     int left=solve(m, n-1);
-
     //     return up+left;
     // }
 }
