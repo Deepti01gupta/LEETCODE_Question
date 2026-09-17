95f981f1a47f54d09b0434d92797894d1fd35e36
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2026-09-18 00:32:41 +0530

    Time: 5 ms (33.05%) | Memory: 45.5 MB (62.27%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,25 +1,54 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        for(int i=1; i<triangle.size(); i++){
-            int k=0;
-            for(int j=0; j<triangle.get(i).size(); j++){
-
-                // upper-side wali value ko calculate krenge
-                int left=j>0? triangle.get(i - 1).get(j - 1) : Integer.MAX_VALUE;
-
-                // upper wali value ko calculate krenge
-                int up=j<triangle.get(i-1).size()? triangle.get(i-1).get(j):Integer.MAX_VALUE;
-
-                // dono k min ko value main add krke wapas store kr denge
-                triangle.get(i).set(j,triangle.get(i).get(j)+Math.min(left,up));
+        int n=triangle.size();
+        int[][] dp=new int[n][n];
+        for(int j=0; j<n; j++){
+            dp[n-1][j]=triangle.get(n-1).get(j);
+        }
+        for(int i=n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                int down=triangle.get(i).get(j) + dp[i+1][j];
+                int right=triangle.get(i).get(j) + dp[i+1][j+1];
+                dp[i][j]=Math.min(down, right);
             }
         }
-
-        // last wali line m min find kr lenge
-        int ans=Integer.MAX_VALUE;
-        for(int val:triangle.get(triangle.size()-1)){
-            ans=Math.min(ans, val);
-        }
-        return ans;
+        return dp[0][0];
     }
+
+
+
+    // public int minimumTotal(List<List<Integer>> triangle) {
+    //     int n=triangle.size();
+    //     int[][] dp=new int[n][n];
+    //     for(int[] r:dp){
+    //         Arrays.fill(r, -1);
+    //     }
+    //     return solve(0, 0, triangle, dp);
+    // }
+    // public int solve(int i, int j, List<List<Integer>> ll, int[][] dp){
+    //     if(i==ll.size()-1){
+    //         return ll.get(i).get(j);
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     int down=ll.get(i).get(j) + solve(i+1, j, ll, dp);
+    //     int right=ll.get(i).get(j) + solve(i+1, j+1, ll, dp);
+    //     return dp[i][j]=Math.min(down, right);
+    // }
+
+
+
+    // public int minimumTotal(List<List<Integer>> triangle) {
+    //     int n=triangle.size();
+    //     return solve(0, 0, triangle);
+    // }
+    // public int solve(int i, int j, List<List<Integer>> ll){
+    //     if(i==ll.size()-1){
+    //         return ll.get(i).get(j);
+    //     }
+    //     int down=ll.get(i).get(j) + solve(i+1, j, ll);
+    //     int right=ll.get(i).get(j) + solve(i+1, j+1, ll);
+    //     return Math.min(down, right);
+    // }
 }
