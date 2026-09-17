478d7aa5194e2f7ac95a6a89dd62ae41b27bd988
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2026-09-17 23:47:44 +0530

    Time: 5 ms (24.24%) | Memory: 50.3 MB (38.96%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,35 +1,71 @@
 class Solution {
+
     public int minPathSum(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int[][] dp=new int[n][m];
+        int[] prev=new int[m];
         for(int i=0; i<n; i++){
+            int[] cur=new int[m];
             for(int j=0; j<m; j++){
                 if(i==0 && j==0){
-                    dp[i][j]=grid[i][j];
+                    cur[j]=grid[i][j];
                 }
                 else{
                     int up=grid[i][j];
                     int left=grid[i][j];
                     if(i>0){
-                        up+=dp[i-1][j];
+                        up+=prev[j];
                     }
                     else{
                         up=Integer.MAX_VALUE;
                     }
                     if(j>0){
-                        left+=dp[i][j-1];
+                        left+=cur[j-1];
                     }
                     else{
                         left=Integer.MAX_VALUE;
                     }
 
-                    dp[i][j]=Math.min(left, up);
+                    cur[j]=Math.min(left, up);
                 }
             }
+            prev=cur;
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
+
+
+
+    // public int minPathSum(int[][] grid) {
+    //     int n=grid.length;
+    //     int m=grid[0].length;
+    //     int[][] dp=new int[n][m];
+    //     for(int i=0; i<n; i++){
+    //         for(int j=0; j<m; j++){
+    //             if(i==0 && j==0){
+    //                 dp[i][j]=grid[i][j];
+    //             }
+    //             else{
+    //                 int up=grid[i][j];
+    //                 int left=grid[i][j];
+    //                 if(i>0){
+    //                     up+=dp[i-1][j];
+    //                 }
+    //                 else{
+    //                     up=Integer.MAX_VALUE;
+    //                 }
+    //                 if(j>0){
+    //                     left+=dp[i][j-1];
+    //                 }
+    //                 else{
+    //                     left=Integer.MAX_VALUE;
+    //                 }
+    //                 dp[i][j]=Math.min(left, up);
+    //             }
+    //         }
+    //     }
+    //     return dp[n-1][m-1];
+    // }
 
 
 
