cbce9d2ee4da9c6c623aee68572fcf6cbac309f4
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2026-09-23 15:16:17 +0530

    Time: 2 ms (70.62%) | Memory: 45.3 MB (36.09%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,50 +1,48 @@
 class Solution {
     
-
-    // public int maxProfit(int[] prices) {
-    //     int n=prices.length;
-    //     int[][] prev=new int[2][3];
-        
-    //     for(int idx=n-1; idx>=0; idx--){
-    //         int[][] cur=new int[2][3];
-    //         for(int turn=0; turn<=1; turn++){
-    //             for(int cap=1; cap<=2; cap++){
-    //                 int ans=0;
-    //                 if(turn==1){
-    //                     ans=Math.max(-prices[idx] + prev[0][cap] , prev[1][cap]);
-    //                 }
-    //                 else{
-    //                     ans=Math.max(prices[idx] + prev[1][cap-1] , prev[0][cap]);
-    //                 }
-    //                 cur[turn][cap]=ans;
-    //             }
-    //         }
-    //         prev=cur;
-    //     }
-    //     return prev[1][2];
-    // }
-
-
-
     public int maxProfit(int k, int[] prices) {
         int n=prices.length;
-        int[][][] dp=new int[n+1][2][k+1];
+        int[][] prev=new int[2][k+1];   
         for(int idx=n-1; idx>=0; idx--){
+            int[][] cur=new int[2][k+1];
             for(int turn=0; turn<=1; turn++){
                 for(int cap=1; cap<=k; cap++){
                     int ans=0;
                     if(turn==1){
-                        ans=Math.max(-prices[idx] + dp[idx+1][0][cap] , dp[idx+1][1][cap]);
+                        ans=Math.max(-prices[idx] + prev[0][cap] , prev[1][cap]);
                     }
                     else{
-                        ans=Math.max(prices[idx] + dp[idx+1][1][cap-1] , dp[idx+1][0][cap]);
+                        ans=Math.max(prices[idx] + prev[1][cap-1] , prev[0][cap]);
                     }
-                    dp[idx][turn][cap]=ans;
+                    cur[turn][cap]=ans;
                 }
             }
+            prev=cur;
         }
-        return dp[0][1][k];
+        return prev[1][k];
     }
+
+
+
+    // public int maxProfit(int k, int[] prices) {
+    //     int n=prices.length;
+    //     int[][][] dp=new int[n+1][2][k+1];
+    //     for(int idx=n-1; idx>=0; idx--){
+    //         for(int turn=0; turn<=1; turn++){
+    //             for(int cap=1; cap<=k; cap++){
+    //                 int ans=0;
+    //                 if(turn==1){
+    //                     ans=Math.max(-prices[idx] + dp[idx+1][0][cap] , dp[idx+1][1][cap]);
+    //                 }
+    //                 else{
+    //                     ans=Math.max(prices[idx] + dp[idx+1][1][cap-1] , dp[idx+1][0][cap]);
+    //                 }
+    //                 dp[idx][turn][cap]=ans;
+    //             }
+    //         }
+    //     }
+    //     return dp[0][1][k];
+    // }
     
 
 
