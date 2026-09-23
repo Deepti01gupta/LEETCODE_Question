a2f09ba0d2df25419761906d6ecdac1100b34c6d
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2026-09-23 15:19:44 +0530

    Time: 17 ms (50.96%) | Memory: 70.7 MB (47.16%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,26 +1,48 @@
 class Solution {
 
     public int maxProfit(int[] prices, int fee) {
-        int[][] dp=new int[prices.length][2];
-        for(int[] r:dp) Arrays.fill(r, -1);
-        return solve(0, 1, fee, prices, dp);
+        int n=prices.length;
+        int[][] dp=new int[n+1][2];
+        dp[n][0]=0;
+        dp[n][1]=0;
+        for(int idx=n-1; idx>=0; idx--){
+            for(int turn=0; turn<=1; turn++){
+                int ans=0;
+                if(turn==1){
+                    ans=Math.max(-prices[idx] + dp[idx+1][0] , dp[idx+1][1]);
+                }
+                else{
+                    ans=Math.max(prices[idx] - fee + dp[idx+1][1] , dp[idx+1][0]);
+                }
+                dp[idx][turn]=ans;
+            }
+        }
+        return dp[0][1];
     }
-    public int solve(int idx, int turn, int fee, int[] arr, int[][] dp){
-        if(idx==arr.length){
-            return 0;
-        }
-        if(dp[idx][turn]!=-1){
-            return dp[idx][turn];
-        }
-        int ans=0;
-        if(turn==1){
-            ans=Math.max(-arr[idx] + solve(idx+1, 0, fee, arr, dp) , solve(idx+1, 1, fee, arr, dp));
-        }
-        else{
-            ans=Math.max(arr[idx] - fee + solve(idx+1, 1, fee, arr, dp) , solve(idx+1, 0, fee, arr, dp));
-        }
-        return dp[idx][turn]=ans;
-    }
+
+
+
+    // public int maxProfit(int[] prices, int fee) {
+    //     int[][] dp=new int[prices.length][2];
+    //     for(int[] r:dp) Arrays.fill(r, -1);
+    //     return solve(0, 1, fee, prices, dp);
+    // }
+    // public int solve(int idx, int turn, int fee, int[] arr, int[][] dp){
+    //     if(idx==arr.length){
+    //         return 0;
+    //     }
+    //     if(dp[idx][turn]!=-1){
+    //         return dp[idx][turn];
+    //     }
+    //     int ans=0;
+    //     if(turn==1){
+    //         ans=Math.max(-arr[idx] + solve(idx+1, 0, fee, arr, dp) , solve(idx+1, 1, fee, arr, dp));
+    //     }
+    //     else{
+    //         ans=Math.max(arr[idx] - fee + solve(idx+1, 1, fee, arr, dp) , solve(idx+1, 0, fee, arr, dp));
+    //     }
+    //     return dp[idx][turn]=ans;
+    // }
 
 
     
