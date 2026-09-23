3f1014bdfb92ebd624272e822b6888934b6dde16
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2026-09-23 15:14:54 +0530

    Time: 6 ms (41.62%) | Memory: 45.5 MB (28.50%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,30 +1,97 @@
 class Solution {
+    
+
+    // public int maxProfit(int[] prices) {
+    //     int n=prices.length;
+    //     int[][] prev=new int[2][3];
+        
+    //     for(int idx=n-1; idx>=0; idx--){
+    //         int[][] cur=new int[2][3];
+    //         for(int turn=0; turn<=1; turn++){
+    //             for(int cap=1; cap<=2; cap++){
+    //                 int ans=0;
+    //                 if(turn==1){
+    //                     ans=Math.max(-prices[idx] + prev[0][cap] , prev[1][cap]);
+    //                 }
+    //                 else{
+    //                     ans=Math.max(prices[idx] + prev[1][cap-1] , prev[0][cap]);
+    //                 }
+    //                 cur[turn][cap]=ans;
+    //             }
+    //         }
+    //         prev=cur;
+    //     }
+    //     return prev[1][2];
+    // }
+
+
+
+    // public int maxProfit(int[] prices) {
+    //     int n=prices.length;
+    //     int[][][] dp=new int[n+1][2][3];
+        
+    //     for(int idx=n-1; idx>=0; idx--){
+    //         for(int turn=0; turn<=1; turn++){
+    //             for(int cap=1; cap<=2; cap++){
+    //                 int ans=0;
+    //                 if(turn==1){
+    //                     ans=Math.max(-prices[idx] + dp[idx+1][0][cap] , dp[idx+1][1][cap]);
+    //                 }
+    //                 else{
+    //                     ans=Math.max(prices[idx] + dp[idx+1][1][cap-1] , dp[idx+1][0][cap]);
+    //                 }
+    //                 dp[idx][turn][cap]=ans;
+    //             }
+    //         }
+    //     }
+    //     return dp[0][1][2];
+    // }
+    
+
+
+
     public int maxProfit(int k, int[] prices) {
-        int[][][] dp=new int[prices.length][2][k];
-        for (int i = 0; i < prices.length; i++) {
-            for (int j = 0; j < 2; j++) {
-                for (int l= 0; l < k; l++) {
-                    dp[i][j][l] = -1;
-                }
+        int[][][] dp=new int[prices.length][2][k+1];
+        for(int[][] r:dp){
+            for(int[] c:r){
+                Arrays.fill(c, -1);
             }
         }
-        return profit(prices,0,1,0,k,dp);
+        return solve(0, 1, k, prices, dp);
     }
-    public int profit(int[] arr,int idx,int turn,int cap, int k, int[][][]dp){
-        if(idx>=arr.length || cap==k){
+    public int solve(int idx, int turn, int cap, int[] arr, int[][][] dp){
+        if(idx==arr.length || cap==0){
             return 0;
         }
         if(dp[idx][turn][cap]!=-1){
             return dp[idx][turn][cap];
         }
-        int buy=0;
-        int sell=0;
+        int ans=0;
         if(turn==1){
-            buy= Math.max(-arr[idx]+profit(arr,idx+1,0,cap,k,dp),profit(arr,idx+1,1,cap,k,dp));
+            ans=Math.max(-arr[idx] + solve(idx+1, 0, cap, arr, dp) , solve(idx+1, 1, cap, arr, dp));
         }
         else{
-            sell= Math.max(arr[idx]+profit(arr,idx+1,1,cap+1,k,dp),profit(arr,idx+1,0,cap,k,dp));
+            ans=Math.max(arr[idx] + solve(idx+1, 1, cap-1, arr, dp) , solve(idx+1, 0, cap, arr, dp));
         }
-        return dp[idx][turn][cap]=Math.max(buy,sell);
+        return dp[idx][turn][cap]=ans;
     }
+
+
+
+    // public int maxProfit(int k, int[] prices) {
+    //     return solve(0, 1, k, prices);
+    // }
+    // public int solve(int idx, int turn, int cap, int[] arr){
+    //     if(idx==arr.length || cap==0){
+    //         return 0;
+    //     }
+    //     int ans=0;
+    //     if(turn==1){
+    //         ans=Math.max(-arr[idx] + solve(idx+1, 0, cap, arr) , solve(idx+1, 1, cap, arr));
+    //     }
+    //     else{
+    //         ans=Math.max(arr[idx] + solve(idx+1, 1, cap-1, arr) , solve(idx+1, 0, cap, arr));
+    //     }
+    //     return ans;
+    // }
 }
