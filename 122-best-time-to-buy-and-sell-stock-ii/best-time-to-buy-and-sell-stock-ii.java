5b268d0dd5ac66f8f98822359d17ffb2b76431bc
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2026-09-23 14:38:53 +0530

    Time: 4 ms (10.31%) | Memory: 50.3 MB (6.25%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,41 +1,42 @@
 class Solution {
-    // public int maxProfit(int[] prices) {
-    //     // int sum=0;
-    //     int max=0;
-    //     for(int i=1; i<prices.length; i++){
-    //         if (prices[i] > prices[i - 1]) {
-    //             max += prices[i] - prices[i - 1];
-    //         }
-    //     }
-    //     return max;
-    // }
+
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-
-        int[][] dp=new int[n+1][2];
-        for(int[] r:dp) Arrays.fill(r,-1);
-
-        return solve(0,1,n,prices,dp);
+        int[][] dp=new int[prices.length][2];
+        for(int[] r:dp) Arrays.fill(r, -1);
+        return solve(0, 1, prices, dp);
     }
-
-    private int solve(int idx, int turn, int n, int[] arr, int[][] dp){
-        if(idx==n){
+    public int solve(int idx, int turn, int[] arr, int[][] dp){
+        if(idx==arr.length){
             return 0;
         }
-
         if(dp[idx][turn]!=-1){
             return dp[idx][turn];
         }
-
-        int buy=0;
-        int sell=0;
+        int ans=0;
         if(turn==1){
-            buy=Math.max(-arr[idx]+solve(idx+1,0,n,arr,dp) , solve(idx+1,1,n,arr,dp));
+            ans=Math.max(-arr[idx] + solve(idx+1, 0, arr, dp) , solve(idx+1, 1, arr, dp));
         }
         else{
-            sell=Math.max(+arr[idx]+solve(idx+1,1,n,arr,dp) , solve(idx+1,0,n,arr,dp));
+            ans=Math.max(arr[idx] + solve(idx+1, 1, arr, dp) , solve(idx+1, 0, arr, dp));
         }
-
-        return dp[idx][turn]=Math.max(buy,sell);
+        return dp[idx][turn]=ans;
     }
+
+
+    // public int maxProfit(int[] prices) {
+    //     return solve(0, 1, prices);
+    // }
+    // public int solve(int idx, int turn, int[] arr){
+    //     if(idx==arr.length){
+    //         return 0;
+    //     }
+    //     int ans=0;
+    //     if(turn==1){
+    //         ans=Math.max(-arr[idx] + solve(idx+1, 0, arr) , solve(idx+1, 1, arr));
+    //     }
+    //     else{
+    //         ans=Math.max(arr[idx] + solve(idx+1, 1, arr) , solve(idx+1, 0, arr));
+    //     }
+    //     return ans;
+    // }
 }
