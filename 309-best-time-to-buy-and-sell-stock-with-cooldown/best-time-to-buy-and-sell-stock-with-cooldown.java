44631430dbaa202ab9e7c085c8a4ab0ddfa18f3b
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2026-09-23 15:32:12 +0530

    Time: 1 ms (69.06%) | Memory: 43.5 MB (64.34%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,29 +1,42 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int[][] dp=new int[prices.length][2];
-        for (int i = 0; i < prices.length; i++) {
-            for (int j = 0; j < 2; j++) {
-                dp[i][j]=-1;
-            }
-        }
-        return profit(prices,0,1,dp);
+        for(int[] r:dp) Arrays.fill(r, -1);
+        return solve(0, 1, prices, dp);
     }
-    
-    public int profit(int[] arr,int idx,int turn,int[][]dp){
+    public int solve(int idx, int turn, int[] arr, int[][] dp){
         if(idx>=arr.length){
             return 0;
         }
         if(dp[idx][turn]!=-1){
             return dp[idx][turn];
         }
-        int buy=0;
-        int sell=0;
+        int ans=0;
         if(turn==1){
-            buy= Math.max(-arr[idx]+profit(arr,idx+1,0,dp),profit(arr,idx+1,1,dp));
+            ans=Math.max(-arr[idx] + solve(idx+1, 0, arr, dp) , solve(idx+1, 1, arr, dp));
         }
         else{
-            sell= Math.max(arr[idx]+profit(arr,idx+2,1,dp),profit(arr,idx+1,0,dp));
+            ans=Math.max(arr[idx] + solve(idx+2, 1, arr, dp) , solve(idx+1, 0, arr, dp));
         }
-        return dp[idx][turn]=Math.max(buy,sell);
+        return dp[idx][turn]=ans;
     }
+
+
+
+    // public int maxProfit(int[] prices) {
+    //     return solve(0, 1, prices);
+    // }
+    // public int solve(int idx, int turn, int[] arr){
+    //     if(idx>=arr.length){
+    //         return 0;
+    //     }
+    //     int ans=0;
+    //     if(turn==1){
+    //         ans=Math.max(-arr[idx] + solve(idx+1, 0, arr) , solve(idx+1, 1, arr));
+    //     }
+    //     else{
+    //         ans=Math.max(arr[idx] + solve(idx+2, 1, arr) , solve(idx+1, 0, arr));
+    //     }
+    //     return ans;
+    // }
 }
